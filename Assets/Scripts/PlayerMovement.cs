8d7ede6390abb01e68eecb5b9916143fedfe049f
lowieCOOL <lowie.vang@gmail.com>
2021-05-15 22:34:42 +0200

    Added simple floor and player to the Gameplay Scene. Added player movement and rotation. Added Cinemachine package and locked camera to player.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float MovementSpeed = 7.5f;
+    [SerializeField] float RotationSpeed = 7.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        RotatePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * MovementSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * MovementSpeed;
+
+        transform.Translate(xValue, 0, zValue);
+    }
+
+    void RotatePlayer()
+    {
+        float xValue = Input.GetAxis("Mouse Y");
+        float yValue = Input.GetAxis("Mouse X");
+
+        transform.Rotate(0, yValue, 0);
+    }
+}
