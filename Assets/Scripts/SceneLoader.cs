1b336e3f54892b134d744d7e6e1a274afaeed588
lowieCOOL <lowie.vang@gmail.com>
2021-05-15 15:30:47 +0200

    Added 2 more scenes. Added a SceneLoader.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = currentScene + 1;
+        SceneManager.LoadScene(nextScene);
+    }
+
+    public void LoadStartingScene()
+    {
+        int startingSceneIndex = 0;
+        SceneManager.LoadScene(startingSceneIndex);
+    }
+}
